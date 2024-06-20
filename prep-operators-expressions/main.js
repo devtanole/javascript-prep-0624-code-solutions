343401cdd26d0ef6faee6ed307f09eaa44daf13c
Devin Olesen <devintanielu@gmail.com>
2024-06-20 08:26:50 +0000

    complete step 5

--- prep-operators-expressions/main.js
+++ prep-operators-expressions/main.js
@@ -1,5 +1,10 @@
 const number1 = 2;
 const number2 = 4;
 let product = number1 * number2;
-console.log('value of product:', number1 * number2);
+console.log('value of product:', product);
 console.log('type of product:', typeof product);
+const charge = 27;
+const payment = 4.5;
+let amountRemaining = charge - payment;
+console.log('value of amountRemaining:', amountRemaining);
+console.log('type of amountRemaining:', typeof amountRemaining);
