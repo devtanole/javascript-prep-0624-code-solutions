8f053898331ca6f239233ad47d5212511ba37f7b
Devin Olesen <devintanielu@gmail.com>
2024-06-20 23:31:16 +0000

    complete first two steps

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,9 @@
+const person = {
+  firstName: 'Devin',
+  lastName: ' Olesen',
+  hobby: 'Training Muay Thai',
+};
+console.log(person);
+
+const fullName = person.firstName + '' + person.lastName;
+console.log("The person's name is:", fullName);
