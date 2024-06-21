6dbae53ccb2c9a1c2e27b4b7e9fda87bbc1506f2
Devin Olesen <devintanielu@gmail.com>
2024-06-21 00:04:07 +0000

    complete steps 5 and 6 and finish exercise

--- prep-javascript-objects/main.js
+++ prep-javascript-objects/main.js
@@ -11,3 +11,4 @@ const fullName = person.firstName + '' + person.lastName;
 console.log("The person's name is:", fullName);
 console.log("The person's current job is:", person.job);
 console.log("The person's previous job is:", person['previousJob']);
+console.log(person);
