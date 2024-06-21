2f235cbc9f1da90dca52fc1c5506b9eba12533cb
Devin Olesen <devintanielu@gmail.com>
2024-06-21 04:08:29 +0000

    create array and first element

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,4 @@
+const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log(languages);
+const firstElement = languages[0];
+console.log('the first element of the array is', firstElement);
