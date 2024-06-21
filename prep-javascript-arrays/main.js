96ae7873f4c2f68dafc112bdcd7b35f0491e31fc
Devin Olesen <devintanielu@gmail.com>
2024-06-21 05:21:53 +0000

    finish exercise

--- prep-javascript-arrays/main.js
+++ prep-javascript-arrays/main.js
@@ -4,5 +4,10 @@ const firstElement = languages[0];
 console.log('the first element of the array is', firstElement);
 const thirdElement = languages[2];
 console.log('the third element of the array is', thirdElement);
-const length = languages.length;
+let length = languages.length;
 console.log('the array length is', languages.length);
+let lastElement = languages[languages.length - 1];
+console.log(
+  'the last element of the array is',
+  languages[languages.length - 1]
+);
