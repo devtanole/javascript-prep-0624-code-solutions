70413e57425491d104abc258d4c92f225bd9a44e
Devin Olesen <devintanielu@gmail.com>
2024-06-22 10:34:22 +0000

    create function two and three

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -3,3 +3,13 @@ function addTwoNumbers(num1, num2) {
 }
 let sum = addTwoNumbers(3, 4);
 console.log('the sum is', sum);
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+const minutes = convertHoursToMinutes(4);
+console.log(minutes);
+function getGreeting(name) {
+  const getGreeting = 'Hello, ' + name + '!';
+  console.log(getGreeting);
+}
+getGreeting('Devin');
