21ab51948ecb2f9cb598bd1eedae29d6d3c5e926
Devin Olesen <devintanielu@gmail.com>
2024-06-25 18:12:11 +0000

    finish loop2

--- prep-javascript-loops/main.js
+++ prep-javascript-loops/main.js
@@ -1,5 +1,5 @@
 // create your loops here.
-const numArray = [];
+let numArray = [];
 let whileLoop1 = 0;
 while (whileLoop1 < 10) {
   numArray.push(whileLoop1);
@@ -7,3 +7,12 @@ while (whileLoop1 < 10) {
 }
 let whileLoop1Result = whileLoop1;
 console.log('numArray ', numArray);
+
+let numArray2 = [];
+let whileLoop2 = 0;
+while (whileLoop2 < 20) {
+  numArray2.push(whileLoop2);
+  whileLoop2 += 2;
+}
+let whileLoop2Result = whileLoop2;
+console.log('numArray2 ', numArray2);
