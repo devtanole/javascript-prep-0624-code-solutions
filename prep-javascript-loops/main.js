7aacfd8564c29c42304efb70fdb3f156de2e3a27
Devin Olesen <devintanielu@gmail.com>
2024-06-25 17:37:19 +0000

    finish loop1

--- prep-javascript-loops/main.js
+++ prep-javascript-loops/main.js
@@ -1,4 +1,8 @@
 // create your loops here.
 const numArray = [];
 let whileLoop1 = 0;
-while (whileLoop1 < 10) {}
+while (whileLoop1 < 10) {
+  numArray.push(whileLoop1);
+  whileLoop1++;
+}
+console.log('numArray ', numArray);
