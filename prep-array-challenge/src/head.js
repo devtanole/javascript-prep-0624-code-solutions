39490e6c019d0c011b9c5247883421f37ee09cd9
Devin Olesen <devintanielu@gmail.com>
2024-06-27 11:29:40 +0000

    first two

---
+++ prep-array-challenge/src/head.js
@@ -0,0 +1,25 @@
+function head(array) {
+  const input = ['foo', 'bar', 'baz'];
+  const input2 = [1, 2, 3, 4, 5];
+  const input3 = ['false', 'true', 'false', 'true'];
+  const input4 = [];
+
+  return array[0];
+}
+
+function last(array) {
+  const input = ['foo', 'bar', 'baz'];
+  const input2 = [1, 2, 3, 4, 5];
+  const input3 = ['false', 'true', 'false', 'true'];
+  const input4 = [];
+
+  return array[array.length - 1];
+}
+
+function tail(array) {
+  const input = ['foo', 'bar', 'baz'];
+  const input2 = [1, 2, 3, 4, 5];
+  const input3 = ['false', 'true', 'false', 'true'];
+  const input4 = [];
+  const find = [];
+}
