f4e06d2a187e5673062eb8a3ce04d5b66d7ce0ea
Devin Olesen <devintanielu@gmail.com>
2024-07-03 01:21:16 +0000

    finish all except keyValPair

---
+++ prep-object-challenge/src/get-value.js
@@ -0,0 +1,31 @@
+function getValue(object, key) {
+  return object[key];
+}
+
+function setValue(object, key, value) {
+  object[key] = value;
+}
+
+function getKeys(object) {
+  const keyArr = [];
+  for (const key in object) {
+    keyArr.push(key);
+  }
+  return keyArr;
+}
+
+function getValues(object) {
+  const valArr = [];
+  for (const key in object) {
+    valArr.push(object[key]);
+  }
+  return valArr;
+}
+
+function toObject(keyValuePair) {
+  const kvp = [];
+  for (const key in object) {
+    kvp.push(object[object[key]]);
+  }
+  return kvp;
+}
